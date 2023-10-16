df02e9584ed688c39fa298fe4932f9ee24de75cf
gabumwaingo <gabumwaingo@gmail.com>
2023-10-15 19:01:28 -0700

    Task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- creates a view for students with less than 80
+-- and no last meeting for past month
+
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name FROM students WHERE score < 80
+AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
