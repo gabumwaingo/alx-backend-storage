b8365eaa4a3743c66e12317ab1a36720c72e06d9
gabumwaingo <gabumwaingo@gmail.com>
2023-10-15 18:17:49 -0700

    Task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- resets attribute valid_email when email changes
+
+DELIMITER $$
+CREATE TRIGGER reset
+BEFORE UPDATE
+ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+
+DELIMITER;
