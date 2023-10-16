e27133b19153850040584d50710d361ea32bfd12
gabumwaingo <gabumwaingo@gmail.com>
2023-10-15 18:10:48 -0700

    Task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- decreases the quantity of an item when order increases
+
+CREATE TRIGGER decrement
+AFTER INSERT
+ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
